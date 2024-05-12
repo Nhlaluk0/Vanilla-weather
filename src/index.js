b40fe2d5d9bd4d5d78f57d860612a59d46f9cafc
Nhlaluk0 <masianhlaluko@gmail.com>
2024-05-12 09:08:00 +0200

    Injected Javascript and updated the css

--- src/index.js
+++ src/index.js
@@ -14,6 +14,9 @@ function refreshWeather(response) {
 
   let windSpeedElement = document.querySelector("#wind-speed");
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+
+  let iconElement = document.querySelector("#icon");
+  iconElement.innerHTML = `<img src= "${response.data.condition.icon_url}" class="current-temperature-icon" />`;
 }
 
 function search(event) {
